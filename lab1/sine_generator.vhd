3f674ea67a31458cc048c013fb2e4d5a1f9ec3eb
danitoribio <dtoribiobruna@gmail.com>
2024-02-22 16:52:01 +0100

    Implementation of the pipeline

--- lab1/sine_generator.vhd
+++ lab1/sine_generator.vhd
@@ -16,6 +16,8 @@ architecture lab1 of GenSen is
   signal counter     : integer range 0 to 10417  := 0;  -- biggest case when f = 600 so 10417 cycles 
   signal rom_address : unsigned(3 downto 0);            -- address of the ROM
   signal rom_data    : signed(7 downto 0);              -- data of the ROM
+  signal filtered_signal : signed(7 downto 0);          -- filtered signal
+  signal enable: std_logic := '1';                      -- enable signal for the filter
 
   signal EoC : std_logic;  --End of Counter to change to the next address of the ROM
 
@@ -26,6 +28,31 @@ architecture lab1 of GenSen is
       );
   end component;
 
+  component FILTER is
+    generic (
+      a0 : integer := 0;
+      a1 : integer := 0;
+      a2 : integer := 0;
+      a3 : integer := 0;
+      a4 : integer := 0;
+      a5 : integer := 0;
+      a6 : integer := 0;
+      a7 : integer := 0;
+      a8 : integer := 0;
+      a9 : integer := 0;
+      a10 : integer := 0;
+      a11 : integer := 0;
+      a12 : integer := 0
+      );
+      port (
+        Clk     : in  STD_LOGIC;      --100MHz so we can count in 10ns
+        Reset   : in  STD_LOGIC;
+        DataIn  : in  SIGNED (7 downto 0);
+        Enable  : in  STD_LOGIC;
+        DataOut : out SIGNED (7 downto 0)
+        );
+  end component;
+
 begin
   --ROM instance
   ROM_inst : rom
@@ -33,6 +60,29 @@ begin
       time_instance => rom_address,
       sin_value     => rom_data
       );
+  FIR_inst : FILTER
+    generic map(
+      a0=>0, 
+      a1=>2, 
+      a2=>0, 
+      a3=>-15, 
+      a4=>0, 
+      a5=>76, 
+      a6=>128, 
+      a7=>76, 
+      a8=>0, 
+      a9=>-15, 
+      a10=>0, 
+      a11=>2, 
+      a12=>0
+      );
+    port map(
+      Clk     => Clk,
+      Reset   => Reset,
+      DataIn  => rom_data,
+      Enable  => enable, -- This signal will be 1 when a new sine value has to be filtered
+      DataOut => filtered_signal
+    );
 
   -- Multiplexer to select the MaxCount
   -- We compute the period of the signal and 
@@ -80,6 +130,10 @@ begin
   end process;
 
   led <= rom_data;                      --copy the data of the ROM to the led
-  dac <= to_unsigned(128, 8) + unsigned(rom_data);  --We add 128 to the led value to get the dac value
+
+  -- He comentado estos dos outputs porque hay que verlo en la simulacion, creo q la solucion final es el d dac2
+  -- pero asi podemos hacer un plot con la senal original y la senal filtrada
+  -- dac1 <= to_unsigned(128, 8) + unsigned(rom_data);  --We add 128 to the led value to get the dac value
+  -- dac2 <= to_unsigned(128, 8) + unsigned(filtered_signal);  --We add 128 to the led value to get the dac value
 end lab1;
 
--- lab2/filter.vhd
+++ lab2/filter.vhd
@@ -25,9 +25,12 @@ entity FILTER is
 
 end FILTER;
 
+
 architecture lab2 of FILTER is
-
-
+type data1 is array (0 to 12) of signed (7 downto 0); 
+signal aX : data1; -- The flip flops that store the input data, x(n), x(n-1)...
+type data2 is array (0 to 4) of signed (15 downto 0); 
+signal temp : data2; -- The values between each multiplication and addition, where temp(3) is stored in the pipeline
 begin
 -- The computed coefficients with matlab are the followings:
 -- -0.000000000000001485990817575209568847452
@@ -47,5 +50,38 @@ begin
 -- our biggest coefficient is 0.5, so 0.5*2^k < 128; k<log2(128/0.5); k = 8 bits.
 -- After multiplying by 2^8 we get the following coefficients (we will divide by 2^8 in the end by taking the 8 MSB of the result):
 -- 0, 2, 0, -15, 0, 76, 128, 76, 0, -15, 0, 2, 0
+
+process(Clk,Reset)
+  begin
+    if Reset='1' then 
+      aX <= (others=> (others=>'0'));
+      temp(3) <= (others=> '0');   -- Pipeline, see diagram
+    elsif rising_edge(Clk) then 
+      if enable='1' then 
+        aX(0) <= DataIn; -- The values in the registers are updated at the rising edge of the clock
+        aX(1) <= aX(0);         
+        aX(2) <= aX(1); 
+        aX(3) <= aX(2);         
+        aX(4) <= aX(3);
+        aX(5) <= aX(4);         
+        aX(6) <= aX(5);
+        aX(7) <= aX(6);         
+        aX(8) <= aX(7);
+        aX(9) <= aX(8);         
+        aX(10) <= aX(9);
+        aX(11) <= aX(10);         
+        aX(12) <= aX(11);                             
+        temp(3) <= temp(2) + a6*aX(6);   -- Pipeline
+      end if; 
+    end if; 
+  end process; 
+  
+  temp(0) <=           A(0)*aX(0); 
+  temp(1) <= temp(0) + A(1)*aX(1);     
+  --temp(2) <= temp(1) + A(2)*aX(2);     
+  
+  temp(3) <= temp(2) + B(1)*aY(1);         
+  temp(4) <= temp(3) + B(2)*aY(2);  
+  combY   <= temp(4)(17 downto 8); 
 
 end lab2;
